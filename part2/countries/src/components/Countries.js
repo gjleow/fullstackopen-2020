3696f65d8aaecb96a145b01cbe7f68539b2dbaa6
gj <gleow3@gatech.edu>
2020-08-16 01:18:26 +0100

    [GJ] Complete exercise 2.14 countries

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,19 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Weather = ({ weather }) => {
+  return (
+    <>
+      <h3>Weather in {weather.location.name}</h3>
+      <p>
+        <b>temperature: </b>
+        {weather.current.temperature} Celsius
+      </p>
+      {weather.current.weather_icons.length > 0 && (
+        <img src={weather.current.weather_icons[0]} alt="weather icon"></img>
+      )}
+      <p>
+        <b>wind: </b>
+        {weather.current.wind_speed} km/h direction {weather.current.wind_dir}
+      </p>
+    </>
+  );
+};
 
 const Country = ({ country }) => {
   const countryFlag = `${country.name} flag`;
+  const api_key = process.env.REACT_APP_API_KEY;
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://api.weatherstack.com/current", {
+        params: {
+          access_key: api_key,
+          query: country.capital,
+        },
+      })
+      .then((response) => {
+        setWeather(response.data);
+      });
+  }, [api_key, country.capital]);
+
   return (
     <>
       <h2>{country.name}</h2>
       <p>capital {country.capital}</p>
       <p>population {country.population}</p>
-      <h3>languages</h3>
+      <h3>Spoken Languages</h3>
       <ul>
         {country.languages.map((language, index) => (
           <li key={index}>{language.name}</li>
         ))}
       </ul>
       <img src={country.flag} alt={countryFlag} width="300"></img>
+      {weather && <Weather weather={weather} />}
     </>
   );
 };
