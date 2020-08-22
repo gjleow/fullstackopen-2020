83a69156bf4c8ed86c3a1813495ddd76d306b969
gj <gleow3@gatech.edu>
2020-08-22 11:58:16 +0100

    [GJ|part4.2] Complete refactor into separate modules

---
+++ part4/blog/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const blogsRouter = require('./controllers/blogs');
+const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
+const config = require('./utils/config');
+
+logger.info('connecting to', config.MONGODB_URI);
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message);
+  });
+
+app.use(cors());
+app.use(express.static('build'));
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use('/api/blogs', blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,42 +1,11 @@
-const express = require('express');
-
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
+const http = require('http');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
-const middleware = require('./utils/middleware');
-const Blog = require('./models/blog');
+const app = require('./app');
 
-const mongoUrl = config.MONGODB_URI;
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-
-app.use(cors());
-app.use(express.json());
-app.use(middleware.requestLogger);
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then((blogs) => {
-      response.json(blogs);
-    });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result);
-    });
-});
-
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
+const sever = http.createServer(app);
 
 const PORT = config.PORT || 3003;
-app.listen(PORT, () => {
+sever.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
 });
