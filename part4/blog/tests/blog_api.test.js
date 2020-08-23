12a801e4a96705e6c243cb4b9a5d67304af9a576
gj <gleow3@gatech.edu>
2020-08-23 23:09:57 +0100

    [GJ|part4.11] New blog default likes to 0

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -57,6 +57,22 @@ test('a valid blog can be added ', async () => {
   );
 });
 
+test('a new blog without likes default to 0', async () => {
+  const newBlog = {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+  };
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  expect(response.body.likes).toBe(0);
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
