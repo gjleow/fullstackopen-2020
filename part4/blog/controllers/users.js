0315e230be6507df6376c0f15ce089102f1f7207
gj <gleow3@gatech.edu>
2020-08-29 11:53:45 +0100

    [GJ]part4.16] Add create user validation

--- part4/blog/controllers/users.js
+++ part4/blog/controllers/users.js
@@ -6,6 +6,10 @@ usersRouter.post('/', async (request, response, next) => {
   try {
     const { body } = request;
 
+    if (body.password !== undefined && body.password.length < 3) {
+      return response.status(400).json({ error: 'password length must be at least 3 characters long' });
+    }
+
     const saltRounds = 10;
     const passwordHash = await bcrypt.hash(body.password, saltRounds);
 
--- part4/blog/models/user.js
+++ part4/blog/models/user.js
@@ -9,6 +9,7 @@ const userSchema = mongoose.Schema({
   username: {
     type: String,
     unique: true,
+    minlength: 3,
   },
   name: String,
   passwordHash: String,
